89944e2c1894f4ea0dedc4fd0de4369ef12c899d
call-me-bhavana <lnerella1s@semo.edu>
2022-12-03 15:33:46 -0600

    modified:   dnode.java

--- src/dnode.java
+++ src/dnode.java
@@ -16,3 +16,46 @@ class dnode
         prev = null;
     }
 }
+
+class dlist
+{
+    private dnode head;
+    private dnode temp;
+    private dnode ptr;
+
+    private dnode ptr1;
+    private dnode ptr2;
+    private dnode dup;
+    public dnode prevn;
+    Scanner sc = new Scanner(System.in);
+
+    public final int strcmp(String a, String b){
+        return a.compareTo(b);
+    }
+    public final void insert()
+    {
+        accept();
+    }
+
+    public final void sort()
+    {
+        dnode i;
+        dnode j;
+        int temp;
+        String n = new String(new char[10]);
+        for (i = head;i.next != null;i = i.next)
+        {
+            for (j = i.next;j != null;j = j.next)
+            {
+                temp = strcmp(i.name,j.name);
+                if (temp > 0)
+                {
+                    n = i.name;
+                    i.name = j.name;
+                    j.name = n;
+                }
+            }
+        }
+
+    }
+
